bb32b234789c0ec1a12caa2f35763c6387f29509
Yeonsu1027 <rito1205@naver.com>
2024-02-07 15:52:58 +0900

    상품검색, 정렬

--- node_030_iolist_v2/public/js/product.js
+++ node_030_iolist_v2/public/js/product.js
@@ -1,16 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  /*
-  * table.products 선택자는 상품리스트 화면에서는 유효한 선택자 이다
-  * 하지만 detail, insert 등의 화면에서는 해당 선택자는 없는 상태이다
-  * detail, insert 화면 등에서는 pro_table 객체가 null 인 상태가 된다는 것이다
-  * pro_table 객체가 null 인 상태일때 .add() 등의 method 를 실행하면
-  * null pointer exception 이 발생하고 HTML JS 에서는 이후의 JS 코드가 모두
-  * 무력화 된다(실행이 안된다)
-  *
-  * 그래서 pro_table 객체가 null 일때는 다른 동작을 건너 뛰도록 해주어야 한다
-  * 이때 사용하는 기호가 "객체?" 이러한 코드를 null safe 코드 라고 한다.
-  */
+  const TH_ITEMS = {
+    상품코드: "p_code",
+    상품이름: "p_name",
+    품목: "p_item",
+    규격: "p_std",
+    매입단가: "p_iprice",
+    매출단가: "p_oprice",
+  };
+
+  // href : 현재 화면이 열릴때 서버에 요청한 주소창의 값들
+  // href 값의 일부를 추출하거나, 값을 가공하기 위하여 사용
+  const url = new URL(document.location.href); // http://localhost:3000/products/?p_search=%EC%9E%90%EC%9C%A0&sort=p_name&order=ASC 이게 통채로
+
+  // new URL 사용하면 searchParams.get & set 을쓸수있다
+  // get : 가져오기, set : 새로운값
+
+  const sort = url.searchParams.get("sort"); //주소창에 sort있으면 가져와라
+  const order = url.searchParams.get("order"); // 원래주소창의 sort와order
+
   const pro_table = document.querySelector("table.products");
+  /*
+   * table.products 선택자는 상품리스트 화면에서는 유효한 선택자 이다
+   * 하지만 detail, insert 등의 화면에서는 해당 선택자는 없는 상태이다
+   * detail, insert 화면 등에서는 pro_table 객체가 null 인 상태가 된다는 것이다
+   * pro_table 객체가 null 인 상태일때 .add() 등의 method 를 실행하면
+   * null pointer exception 이 발생하고 HTML JS 에서는 이후의 JS 코드가 모두
+   * 무력화 된다(실행이 안된다)
+   *
+   * 그래서 pro_table 객체가 null 일때는 다른 동작을 건너 뛰도록 해주어야 한다
+   * 이때 사용하는 기호가 "객체?" 이러한 코드를 null safe 코드 라고 한다.
+   */
   pro_table?.addEventListener("click", (e) => {
     const target = e.target;
     if (target.tagName === "TD") {
@@ -18,8 +37,47 @@ document.addEventListener("DOMContentLoaded", () => {
       const p_code = tr.dataset.pcode;
 
       document.location.replace(`/products/${p_code}/detail`);
-    }
-  });
+      // 현재 click 된 요소가 TH 이거나 TH의 자손이면..
+      // th안에 span, i 가있어서 3개중 아무거나 눌러도 th누른걸로 처리하기위함
+    } else if (target.tagName === "TH" || target.closest("TH")) {
+      // 텍스트가져오는코드
+      const text = target.innerText || target.closest("TH").innerText;
+      // = TH_ITEMS.규격 형태 와같다 // TH_ITEMS["규격"] 이런느낌
+      const sortColumn = TH_ITEMS[text.trim()]; // trim 써서 whitespace제거 : 한글문자열에 안보여도 껴있을 수 있어서
+
+      // url 중에서 searchParam(또는 queryString) 들만 추출하기
+      // 주소의 ? 뒤의값을 추출
+      url.searchParams.set("order", order === "ASC" ? "DESC" : "ASC"); // 클릭할때마다 정렬순서 바뀜
+
+      // 주소창의 sort 선택요소와 클릭한 선택요소가 다르면
+      // 무조건 ASC 로 초기화
+      sort != sortColumn && url.searchParams.set("order", "ASC"); // sort는현재주소창내용 솔트칼럼은 선택한거
+
+      // sortColumn 이 null 이 아닌 경우만 sort 변수를 세팅
+      // null safe 코드
+      sortColumn && url.searchParams.set("sort", sortColumn);
+      document.location.replace(`/products?${url.searchParams.toString()}`);
+
+      // 이걸 위에걸로 간편하게
+      // let sort ="p_code"
+      // if(text ==="상품코드") {
+      //   sort ="p_code"
+      // } else if (text ==="상품이름") {
+      //   sort = "p_name"
+      // }
+
+      //alert(sortColumn);
+    } // end if
+  }); // end event
+  // "span.p_code"
+
+  // DOMContentLoaded event 가 실행될때 마다 실행
+  // 화면이 새로고침 될때마다 실행
+  const span_sort = document.querySelector(`span.${sort}`);
+  const icon = span_sort.querySelector("i.arrow");
+
+  span_sort.classList.add("sort");
+  icon.classList.add(order === "ASC" ? "up" : "down");
 });
 
 //기능 분리가 아닌 코드분리
@@ -68,18 +126,19 @@ document.addEventListener("DOMContentLoaded", () => {
   input_img?.addEventListener("change", imagePreView);
 
   // div박스 클릭하면 감춘 input애 포커스
-  div_img.addEventListener("click", () => {
+  div_img?.addEventListener("click", () => {
     input_focus.focus();
   });
 
   // 복붙하면 paste 이벤트발생 , e는 클립보드가된다
-  div_img.addEventListener("paste", async (e) => {
+  div_img?.addEventListener("paste", async (e) => {
     const items = e.clipboardData.items;
     const item = items[0]; //클립보드에저장된 첫번째가
-    const img_add = document.querySelector("img.img_add"); 
+    const img_add = document.querySelector("img.img_add");
     const input_image = document.querySelector("#p_image");
 
-    if (item.type.indexOf("image") === 0) { //이미지니? 0이면 img
+    if (item.type.indexOf("image") === 0) {
+      //이미지니? 0이면 img
       const blob = item.getAsFile(); // 그복붙한이미지를 파일로..
       if (!blob) return false;
 
--- node_030_iolist_v2/routes/products.js
+++ node_030_iolist_v2/routes/products.js
@@ -3,6 +3,9 @@ import DB from "../models/index.js";
 
 import { upLoad } from "../modules/file_upload.js"; //{}만쳐도 바로 from 자동작성
 
+//sequelize 를 사용할때 추가로 제공되는 확장 연산자
+import { Op } from "sequelize";
+
 const PRODUCTS = DB.models.tbl_products;
 const IOLIST = DB.models.tbl_iolist;
 const DEPTS = DB.models.tbl_depts;
@@ -10,9 +13,29 @@ const DEPTS = DB.models.tbl_depts;
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const rows = await PRODUCTS.findAll({ limit: 10, order: [["p_code", "ASC"]] }); //await 중요!! 담기는거 기다려야함 //
-  return res.render("product/list", { PRODUCTS: rows }); // limit: 몇개까지만 보이게
+  const p_search = req.query.p_search || ""; // p_search 에담긴 검색어/ 없으면 빈칸취급
+  // "" : WHERE p_name LIKE '%%'; -- 전체데이터 셀렉트
+  const sort = req.query.sort || "p_code"; // sort가 없으면 상품코드로검색
+  const order = req.query.order || "ASC"; // order 가없으면 ASC로검색 :기본값을 지정해준다
+
+  const rows = await PRODUCTS.findAll({
+    where: {
+      [Op.or]: [
+        { p_name: { [Op.like]: `%${p_search}%` } }, // like 연산자 식
+        { p_code: `${p_search}` },
+      ],
+    },
+    order: [[sort, order]],
+  }); //await 중요!! 담기는거 기다려야함 //
+  return res.render("product/list", { PRODUCTS: rows, p_search }); // order 앞에 limit: 10, limit: 몇개까지만 보이게
+  // p_serarch 를 다시 렌더한테 보내준다 : 검색창에 입력한 검색어가 남게하기위함
 });
+
+// router.post("/", async (req, res) => {
+//   // 상품 이름을 검색해서..???
+//   const rows = await PRODUCTS.findAll({ order: [["p_name"]] });
+//   return res.json({ rows });
+// });
 
 // router.get("/input", (req, res) => {
 //   return res.render("product/input");
@@ -22,15 +45,84 @@ router.get("/insert", async (req, res) => {
   return res.render("product/input");
 }); //상품추가페이지
 
+/**
+ * 상품코드는 1자리의 Prifix(P)와 5자리의 연속된 일련번호 형식
+ * 상품코드는 중복되면 절대 안되고,빈(blank, empty) 값도 안된다
+ * 규칙이 자릿수가 일정한 형태
+ *
+ * 새로운 상품코드를 생성하기 위하여
+ * 1. 기존의 DB Table 에서 가장 큰 상품코드 값을 추출하기
+ * 2. Prefix 를 분리
+ * 3. 숫자 부분을 분리
+ * 4. 숫자부분의 문자열을 숫자로 변경하고 +1 을 실행
+ * 5. Prefix 와 숫자부분을 결합하여 코드로 생성
+ * 6. 숫자부분의 자릿수를 맞추고 공백부분은 0으로 채워넣어야 한다
+ */
+const makePCode = (pcode) => {
+  const pCodePrefix = pcode.substring(0, 1); //P
+  let pCodeNum = pcode.substring(1); // 1번 index 이후의 값(숫자부분만) //얘가14면
+  const pCodeNumLength = pCodeNum.length; // 문자열 길이추출
+
+  pCodeNum = String(Number(pCodeNum) + 1); // 얜15 : 문자로바꿈
+  pCodeNum = "0000000000" + pCodeNum;
+  // pCodeNum.length = 10 + 숫자 자릿수  // 숫자가 15면 현재 12자리
+  // pCodeNum 의 전체 길이에서 원래 코드 숫자부분의 길이 만큼 뺀 위치부터
+  // 문자열 잘라내기
+  pCodeNum = pCodeNum.substring(pCodeNum.length - pCodeNumLength);
+
+  return `${pCodePrefix}${pCodeNum}`;
+};
+
+const makePCodeNew = (pcode) => {
+  const pCodePrefix = pcode.substring(0, 1);
+  let pCodeNum = pcode.substring(1);
+  const pCodeNumLength = pCodeNum.length;
+
+  pCodeNum = String(Number(pCodeNum) + 1);
+  /**
+   * 문자열.padStart(길이, 패턴)
+   *
+   * 문자열 값을 전체 "길이" 개수만큼 만들고
+   * 왼쪽에 비어있는 곳은 "패턴"으로 채워넣은 문자열을 생성하라
+   *
+   */
+  // (전체자릿수)length만큼 0을 채워넣어라 Start:앞에다가 / padEnd 도있음:뒤에부터채워라
+  pCodeNum = pCodeNum.padStart(pCodeNumLength, "0");
+  return `${pCodePrefix}${pCodeNum}`;
+};
+
 // 중간에 파일이 있으면 가로채서 파일업로드 작업수행
 // p_image 는 pug의 img input tag의 name
 // upLoad 미들웨어의 single()함수
-router.post("/insert", upLoad.single("p_image"), (req, res) => {
+router.post("/insert", upLoad.single("p_image"), async (req, res) => {
   // single : 하나의 이미지만 받겠다
 
-  const file = req.file;
+  // 중복검사대신 새코드를 생성한다
+  let pCode = req.body.p_code;
+  if (pCode === "000") {
+    // findeAll() 을 실행한 결과는 비록 SELECT 된 결과가 0개또는 1개 뿐이지만
+    // 결과는 배열(List) type 이다
+    const rows = await PRODUCTS.findAll({ order: [["p_code", "DESC"]], limit: 1 }); // pcode 역순정렬해서 1개만 추출(한개여도배열)
+    pCode = rows[0].p_code; // 0번위치코드 : 마지막 p_code
+    pCode = makePCodeNew(pCode); //make에 마지막 p_code 전달해서 거기에+1 된 새로운 p_code 받기
+    req.body.p_code = pCode; // 상품코드입력에 코드 집어넣기
+  }
 
-  return res.json({ body: req.body, file });
+  const file = req.file;
+  // 사진을 넣으면(사진이있으면)
+  if (file) {
+    // 사진파일 집어넣기
+    req.body.p_image_name = file.filename;
+    req.body.p_image_origin_name = file.originalname;
+  }
+
+  try {
+    await PRODUCTS.create(req.body); // 데이터추가명령
+    return res.redirect("/products/"); // 리스트로 점프 추가된거보여주기
+  } catch (error) {
+    return res.json(error);
+  }
+  // return res.json({ body: req.body, file });
 });
 
 router.get("/:pcode/detail", async (req, res) => {
@@ -40,7 +132,7 @@ router.get("/:pcode/detail", async (req, res) => {
   const row = await PRODUCTS.findByPk(pcode); // 디테일용 직접쓴거..!!!!!
   // 상품코드 정보를 가져와서 가지고 iolist한테가서 join 하여 쓸 수있게한다..
   // FK있으면 가능한코드
-  return res.render("product/detail2", { PRODUCT: row });
+  return res.render("product/detail", { PRODUCT: row });
 });
 
 // // 수정만들어보기.. 일단 수정하는 곳으로 보내고
